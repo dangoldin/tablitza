3e7bb0a40dfd9009b5d61fac67361d057c7b28d1
Dan Goldin <dangoldin@gmail.com>
2017-03-17 22:54:41 -0400

    More minor work

--- src/app.js
+++ src/app.js
@@ -1,53 +1,68 @@
+"use strict";
+
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-import SpreadsheetComponent from 'react-spreadsheet-component';
+class Cell extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <td>
+                <input type="text" value={this.props.value} />
+            </td>
+        )
+    }
+}
+
+class Spreadsheet extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        const Rows = this.props.spreadsheet.map(function(row) {
+            const Row = row.map(function(cell) {
+                return <Cell value={cell} />
+            })
+            return (
+                <tr>
+                    {Row}
+                </tr>
+            )
+        })
+        return (
+            <table>
+                <tbody>
+                    {Rows}
+                </tbody>
+            </table>
+        )
+    }
+}
 
 class Main extends Component {
     constructor(props){
-        super();
+        super(props)
     }
 
     render(){
-        const initialData = {
-            rows: [
-                ['Key', 'AAA', 'BBB', 'CCC', 'DDD', 'EEE', 'FFF', 'GGG'],
-                ['COM', '0,0', '0,1', '0,2', '0,3', '0,4', '0,5', '0,6'],
-                ['DIV', '1,0', '1,1', '1,2', '1,3', '1,4', '1,5', '1,6'],
-                ['DEV', '2,0', '2,1', '2,2', '2,3', '2,4', '2,5', '2,6'],
-                ['ACC', '3,0', '3,1', '3,2', '3,3', '3,4', '3,5', '3,6']
-            ]
-        };
-
         const config = {
-            // Initial number of row
-            rows: 5,
-            // Initial number of columns
-            columns: 8,
-            // True if the first column in each row is a header (th)
-            hasHeadColumn: true,
-            // True if the data for the first column is just a string.
-            // Set to false if you want to pass custom DOM elements.
-            isHeadColumnString: true,
-            // True if the first row is a header (th)
-            hasHeadRow: true,
-            // True if the data for the cells in the first row contains strings.
-            // Set to false if you want to pass custom DOM elements.
-            isHeadRowString: true,
-            // True if the user can add rows (by navigating down from the last row)
-            canAddRow: true,
-            // True if the user can add columns (by navigating right from the last column)
-            canAddColumn: true,
-            // Override the display value for an empty cell
-            emptyValueSymbol: '-',
-            // Fills the first column with index numbers (1...n) and the first row with index letters (A...ZZZ)
-            hasLetterNumberHeads: true
-        };
+            'row': 5,
+            'col': 5
+        }
+
+        const data = [
+            [1,2],
+            [3,4]
+        ]
 
         return (
             <div>
                 <h1>Tablitza</h1>
-                <SpreadsheetComponent initialData={initialData} config={config} spreadsheetId="1" />, document.getElementsByTagName('body'));
+                <Spreadsheet spreadsheet={data} />
             </div>
         )
     }
