c6f8d2cd63e1fdaff46f5a204aae016e674a414e
Dan Goldin <dgoldin@triplelift.com>
2017-04-13 23:28:45 -0400

    Get add row and col working

--- src/app.js
+++ src/app.js
@@ -9,32 +9,111 @@ class Main extends Component {
     constructor(props){
         super(props)
 
+        const addRowCell = {value: '', readOnly: true, component: (
+                    <button onClick={() => this.addRow() }>
+                        +
+                    </button>
+                ), forceComponent: true};
+
+        const addColCell = {value: '', readOnly: true, component: (
+                    <button onClick={() => this.addCol() }>
+                        +
+                    </button>
+                ), forceComponent: true}
+
         this.state = {
             config: {
-                'row': 5,
-                'col': 5
+                'rows': 10,
+                'cols': 5
             },
             data: [
-                [{value: '', readOnly: true}, {value: 'A', readOnly: true}, {value: '+', readOnly: true}],
+                [{value: '', readOnly: true}, {value: 'A', readOnly: true}, addColCell],
                 [{value: 1, readOnly: true},{value: 4}],
                 [{value: 2, readOnly: true},{value: 6}],
                 [{value: 3, readOnly: true},{value: 8}],
                 [{value: 4, readOnly: true},{value: 10}],
-                [{value: '', readOnly: false, component: (
-                    <button onClick={() => this.insertRow() }>
-                        +
-                    </button>
-                ), forceComponent: true}]
+                [addRowCell]
             ]
         }
     }
 
-    insertRow() {
+    getData() {
+        let config = this.state.config;
         let data = this.state.data;
-        data.push([{value: '', readOnly: false}]);
+        const rows = config.rows;
+        const cols = config.cols;
+
+        const newRows = data.map((row, idx) => {
+            // First row
+            if (idx == 0) {
+                const vals = row;
+
+                for (var i = row.length - 1; i < cols; i++) {
+                    // TODO: Go past 26
+                    const char = String.fromCharCode(65 + i);
+                    vals[i] = {value: char, readOnly: true};
+                }
+
+                return vals;
+            }
+
+            const vals = row;
+            for (var i = row.length - 1; i < cols; i++) {
+                vals[i] = {value: ''};
+            }
+            return vals;
+        });
+
+        newRows.pop(); // This is the old addRow
+
+        const emptyRow = [];
+        for (var i = 0; i < cols; i++) {
+            emptyRow[i] = {value: ''};
+        }
+
+        for (var i = data.length - 1; i < rows; i++) {
+            const newRow = Array.from(emptyRow);
+            newRow[0] = {value: i, readOnly: true};
+
+            newRows.push(newRow);
+        }
+
+        // addRow option
+        newRows[newRows.length - 1][0] = {
+            value: '', readOnly: true, component: (
+                    <button onClick={() => this.addRow() }>
+                        +
+                    </button>
+                ), forceComponent: true}
+
+        // addCol option
+        newRows[0][cols - 1] = {
+            value: '', readOnly: true, component: (
+                    <button onClick={() => this.addCol() }>
+                        +
+                    </button>
+                ), forceComponent: true}
+
+        return newRows;
+    }
+
+    addRow() {
+        let config = this.state.config;
+        config.rows += 1;
 
         this.setState({
-            data: data
+            config: config,
+            data: this.getData()
+        })
+    }
+
+    addCol() {
+        let config = this.state.config;
+        config.cols += 1;
+
+        this.setState({
+            config: config,
+            data: this.getData()
         })
     }
 
